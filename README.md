98a826185c0f88f03d47ae1b9d6fe0f7e88f77d0
at15 <at15@outlook.com>
2016-10-27 01:03:21 -0700

    [util] Generator is working
    - support links in database

--- README.md
+++ README.md
@@ -1,117 +1,41 @@
+<!-- NOTE: this md file is generated, please don't modify the file directly -->
 # awesome-time-series-database
 A curated list of awesome time series databases, benchmark tools, blog and published papers
 
 ## Contents
 
-- [Time series databases](#time-series-database)
+- [Time series databases](#time-series-databases)
   - [HBase](#hbase)
   - [Cassandra](#cassandra)
   - [Bigtable](#bigtable)
+  - [Memoery](#memoery)
 - [Monitoring solutions](#monitoring-solutions)
-
-aside
-
-aside
-
-aside
-a
-
-side
-
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-
-aside
-
-aside
-
-aside
-a
-
-side
-
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-aside
-
-\aside
-
-aside
-
-aside
-a
-
-side
-
-aside
-
-aside
-
-aside
-a
-
-side
-
-aside
-
-aside
-
-aside
-a
-
-side
-
-
-
-aside
 
 ## Time series databases
 
+NOTE: some databases have multiple backends, they are grouped by their default backend.
+
 ### HBase
 
-
-- [OpenTSDB](http://opentsdb.net/) undefined
-
-
+- [OpenTSDB](http://opentsdb.net/) Classical time series database on top of HBase. Now support Cassandra and Bigtable.
+  - Language: java
+  - Backends: [HBase](#hbase)
 
 ### Cassandra
 
-
-- [KairosDB](https://kairosdb.github.io/) A tsdb
-
-
-
-- [Heroic](https://spotify.github.io/heroic/) A tsdb
-  - [t](https://www.baidu.com)
-
+- [KairosDB](https://kairosdb.github.io/) Fast Time Series Database on Cassandra
+  - Language: java
+  - Backends: [Cassandra](#cassandra)
+- [Heroic](https://spotify.github.io/heroic/) A scalable time series database based on Bigtable, Cassandra, and Elasticsearch
+  - Language: java
+  - Backends: [Cassandra](#cassandra), [HBase](#hbase), [Bigtable](#bigtable)
+  - [Monitoring at Spotify - Introducing Heroic](https://labs.spotify.com/2015/11/17/monitoring-at-spotify-introducing-heroic/)
 
 ### Bigtable
 
-
+### Memoery
 
 ## Monitoring solutions
-
-
 
 ## License
 
--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const yaml = require('js-yaml');
 const hb = require('handlebars');
 
 // TODO: migrate code from https://github.com/tongquhq/about/blob/master/lib/config/parser.js to support $ref in yaml
-
 const backends = yaml.safeLoad(fs.readFileSync('backend.yml','utf-8'));
 const databases = yaml.safeLoad(fs.readFileSync('database.yml','utf-8'));
 
+// console.log(backends);
 
-console.log(backends);
 const data = {
   backends: backends,
   databases: databases
 };
+
+// Handlerbars helpers
 let toRef = function(name) {
   // http://stackoverflow.com/questions/1983648/replace-space-with-dash-and-make-all-letters-lower-case-using-javascript
-  return name.replace(/\s+/g, '-').toLowerCase();
+  return '#' + name.replace(/\s+/g, '-').toLowerCase();
 }
 hb.registerHelper('toRef', toRef);
 hb.registerHelper('db', function(dbName){
@@ -27,17 +28,30 @@ hb.registerHelper('db', function(dbName){
     console.error(dbName, 'is not defined!');
     return '';
   }
-  return `
-- [${dbName}](${toRef(db.url)}) ${db.description}
-${_.map(db.links, (v, k)=> {
-                return `  - [${k}](${v})`;
-            }).join('\n')}
-`
+  let head = '';
+  let language = ';'
+  let backends = '';
+  let links = '';
+  head = `- [${dbName}](${db.url}) ${db.description}`;
+  if (typeof db.language == 'string') {
+    language = '\n  - Language: ' + db.language;
+  }
+  if (typeof db.backends == 'object') {
+    backends = '\n  - Backends: ' + _.map(db.backends, function(b){return `[${b}](${toRef(b)})`}).join(', ');
+  }
+  if (typeof db.links == 'object') {
+    links = '\n' + _.map(db.links, function(url, name){return `  - [${name}](${url})`}).join('\n');
+  }
+  return head + language + backends + links;
 });
+
+// Render and writ to file
 let tmpl = hb.compile(fs.readFileSync('README.tmpl', 'utf-8'));
-// let out = Mustache.render(fs.readFileSync('README.mustache', 'utf-8'), data);
-let out = tmpl(data);
+// FIXME: the template gnerated extra line breaks
+// http://stackoverflow.com/questions/10965433/regex-replace-multi-line-breaks-with-single-in-javascript
+let out = tmpl(data).replace(/\n\s*\n\s*\n/g, '\n\n');
+
+
 
 console.log(out);
-
 fs.writeFileSync('README.md', out);

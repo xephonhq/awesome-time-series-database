a5e5d3ff149e7b4a4b88c8ed4d118181bbda3c54
at15 <at15@outlook.com>
2017-09-02 23:14:25 -0700

    [web] Add twitter card
    - but didn't add twitter share button ....

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,21 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: 'A curated list of awesome time series databases, benchmark tools, blog and papers'
-      }
+      },
+      // TODO: https://dev.twitter.com/web/tweet-button
+      // https://nuxtjs.org/examples/seo-html-head
+      // Test on: https://cards-dev.twitter.com/validator
+      {name: 'twitter:card', content: 'summary'},
+      // TODO: the twitter account for xephon ?
+      {name: 'twitter:site', content: '@at1510086'},
+      {name: 'twitter:title', content: 'Awesome Time Series Database'},
+      {
+        name: 'twitter:description',
+        content: 'A curated list of awesome time series databases, benchmark tools, blog and papers'
+      },
+      // TODO: use our own image url
+      {name: 'twitter:image', content: 'https://awesome-go.com/assets/logo.png'},
+      {name: 'twitter:image:alt', content: 'Awesome logo'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,25 +1,35 @@
 <template>
     <div>
-        <nav class="ui blue basic inverted icon top fixed menu">
+        <nav class="ui black basic inverted icon top fixed menu">
             <a class="item">
                 <i class="sidebar icon"></i>
             </a>
             <nuxt-link to="/" class="item">
                 Awesome Time Series Database
             </nuxt-link>
-            <div class="right menu">
-                <!-- TODO: real client side search -->
-                <a class="item">
-                    <div class="ui icon input">
-                        <input type="text" placeholder="Search  ...">
-                        <i class="search link icon"></i>
-                    </div>
-                </a>
-            </div>
+            <a class="icon item" href="https://github.com/xephonhq/awesome-time-series-database" target="_blank">
+                <i class="ui large github icon"></i>
+            </a>
+            <!--<div class="right menu">-->
+                <!--&lt;!&ndash; TODO: real client side search &ndash;&gt;-->
+                <!--<a class="item">-->
+                    <!--<div class="ui icon input">-->
+                        <!--<input type="text" placeholder="Search  ...">-->
+                        <!--<i class="search link icon"></i>-->
+                    <!--</div>-->
+                <!--</a>-->
+            <!--</div>-->
         </nav>
         <div class="main-container">
             <nuxt></nuxt>
         </div>
+        <!--<div class="ui black inverted footer">-->
+            <!--<div class="ui center aligned container">-->
+                <!--<a href="https://github.com/xephonhq/awesome-time-series-database" target="_blank">-->
+                    <!--<i class="ui large github icon"></i>-->
+                <!--</a>-->
+            <!--</div>-->
+        <!--</div>-->
     </div>
 </template>
 

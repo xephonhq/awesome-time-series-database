a79178cc429ff06ce3d33f4771e9cac6aacc846e
at15 <at15@outlook.com>
2020-01-06 22:04:33 -0800

    Add commit and push

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -3,18 +3,27 @@
 set -e
 
 echo "workspace $GITHUB_WORKSPACE repo $GITHUB_REPOSITORY"
-echo "I am going to deploy to $BRANCH $1"
+echo "arg is $1"
+
+# Build
 git status
 make build
 DIST_PATH="$(pwd)/docs/.vuepress/dist/"
 
+# Clone gh-pages
 # https://github.com/grasilife/github-pages-deploy-action/blob/master/entrypoint.sh#L63
 REPO_PATH="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
 cd /tmp && git clone "${REPO_PATH}" repo
 cd /tmp/repo && git checkout gh-pages && git status
+
+# Sync build
 # https://stackoverflow.com/questions/23698183/how-to-force-cp-to-overwrite-directory-instead-of-creating-another-one-inside
-rsync -avh "${DIST_PATH}" /tmp/repo/
+rsync -avh --delete "${DIST_PATH}" /tmp/repo/
 git status
+
+# Commit and push
+git add -A .
+git commit -m "Deplo from ${GITHUB_SHA}"
+git push "${REPO_PATH}:gh-pages"
 
 echo "deployed"

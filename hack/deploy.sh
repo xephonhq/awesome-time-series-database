06184d6d76f5b4efc035fc2343cbb1d2d6ee1fd3
at15 <at15@outlook.com>
2020-01-06 21:30:42 -0800

    [hack] Add clone repo to /tmp in action

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -2,5 +2,13 @@
 
 set -e
 
+echo "workspace $GITHUB_WORKSPACE repo $GITHUB_REPOSITORY"
 echo "I am going to deploy to $BRANCH $1"
 git status
+
+# https://github.com/grasilife/github-pages-deploy-action/blob/master/entrypoint.sh#L63
+REPO_PATH="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+cd /tmp && git clone "${REPO_PATH}" tr && cd /tmp/tr && git checkout gh-pages
+
+echo "deployed"
